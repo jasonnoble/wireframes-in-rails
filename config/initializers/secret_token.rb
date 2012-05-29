3276fe749a1d61ce88ea3779e72fddd5333a206f
jasnow <jasnow@hotmail.com>
2012-05-29 14:53:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WireframesInRails::Application.config.secret_token = '2244e4f5c640a48977b36ce56a17095161760f0732a9e04274ac7a3547eb3bac21946418fbaa57bef7599378887b6019311e4ad4a6198bd378d3c58d7c378d66'
